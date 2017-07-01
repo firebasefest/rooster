280d112fe49c7e764a2008868bad24d427b880e2
Shaz <shahebaz13@gmail.com>
2017-07-01 15:14:56 +0530

    Added many

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router  = express.Router();
+var firebase = require('firebase');
 
 //root route
 router.get("/", function(req, res){
@@ -12,6 +13,10 @@ router.get("/home", function(req, res){
 
 router.get("/roostie", function(req, res){
     res.render("roostie");
+});
+
+router.get("/confirm", function(req, res){
+    res.render("confirm");
 });
 
 module.exports = router;
